ff701ef73a8092e2160b562748489af0d6e5fcf1
umarovl7 <azamatumarov770@gmail.com>
2025-01-24 15:44:12 +0500

    sneakers

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-const btn = document.querySelector("button");
+// const btn = document.querySelector("button");
 
-function random(number) {
-    return Math.floor(Math.random() * (number + 1));
+// function random(number) {
+   // return Math.floor(Math.random() * (number + 1));
+// }
+// btn.addEventListener("click", () => {
+   // const rndcol = `rgb(${random(235)} ${random(235)} ${random(235)})`;
+   // document.body.style.backgroundColor = rndcol;
+// });
+const sneakers1 = {
+    firstName: "Puma",
+    lastName: "Reebok",
+    price: "$500",
+    color: "black"
 }
-btn.addEventListener("click", () => {
-    const rndcol = `rgb(${random(235)} ${random(235)} ${random(235)})`;
-    document.body.style.backgroundColor = rndcol;
-});
+
+const sneakers2 = {firstName: "Puma" , lastname: "Fashion"};
+
+Object.assign(sneakers1, sneakers2);
+
+let text = Object.entries(sneakers1);
+document.getElementById("sirit").innerHTML = text;
