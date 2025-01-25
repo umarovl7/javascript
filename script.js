a852730de5bac5298b3d2d32c570a0b5a1199bf8
umarovl7 <azamatumarov770@gmail.com>
2025-01-25 18:01:50 +0500

    done

--- script.js
+++ script.js
@@ -7,18 +7,48 @@
    // const rndcol = `rgb(${random(235)} ${random(235)} ${random(235)})`;
    // document.body.style.backgroundColor = rndcol;
 // });
-const sneakers1 = {
-    firstName: "New Balance",
-    lastName: "Converse",
-    price: 300, 
-    color: "brown"
-};
+// const sneakers1 = {
+   // firstName: "New Balance",
+    //lastName: "Converse",
+    // price: 300, 
+  //  color: "brown"
+//};
 
-const sneakers2 = { firstName: "Nike", lastName: "Jordan" }; 
+//const sneakers2 = { firstName: "Nike", lastName: "Jordan" }; 
 
-Object.assign(sneakers1, sneakers2);
+//Object.assign(sneakers1, sneakers2);
 
 
-let text = Object.values(sneakers1); 
+//let text = Object.values(sneakers1); 
 
-document.getElementById("advent").innerHTML = text;
+//document.getElementById("advent").innerHTML = text;
+/////////
+//function Person(first, last, age, hair) {
+  //  this.firstname = first;
+    //this.lastname = last;
+   // this.age = age;
+   // this.hair = hair;
+//}
+
+//Person.prototype.gender = "female";
+
+//const sister = new Person("Nairobi", "Gaztambide", "29", "dark");
+
+//document.getElementById("spirit").innerHTML = 
+  //"The age of my sister is " + sister.age;
+
+
+  function Meal(name, origin, century) {
+    this.name = name;
+    this.origin = origin;
+    this.century = century;
+}
+
+
+Meal.prototype.ingredients = ["rice", "carrots", "onions", "water"];
+
+const meal = new Meal("Palov", "Uzbekistan", "10th");
+
+document.getElementById("meal").innerHTML =
+   "The origin of the meal is " + meal.origin;
+
